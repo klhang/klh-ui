3d4a054b9b7b046cb6376ba70bd7733545a10d46
Klhang <chrisjunhang@gmail.com>
2020-08-04 10:55:17 -0700

    index.tsx can be bundled into index.js by webpack

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'production',
+    entry: {
+        index: './lib/index.tsx'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist/lib'),
+        library: 'KUI',
+        libraryTarget: 'umd',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'awesome-typescript-loader'
+            }
+        ]
+    },
+
+};
